1546b239f9fb8b6c9e9a750f3ddebc8812554748
knowbased <60846047+knowbased@users.noreply.github.com>
2023-08-19 21:47:49 +0200

    chore: install and configure prettier and eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,35 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:import/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react-hooks/recommended",
+
+    // Make sure it's always the last config, so it gets the chance to override other configs.
+    "eslint-config-prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  settings: {
+    react: {
+      // Tells eslint-plugin-react to automatically detect the version of React to use.
+      version: "detect",
+    },
+    // Tells eslint how to resolve imports
+    "import/resolver": {
+      typescript: "true",
+      node: "true",
+    },
+  },
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
