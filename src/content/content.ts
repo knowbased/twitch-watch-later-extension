d85ae31107a8aea5f3e7fc046fdb91ab0b9a6a0d
knowbased <60846047+knowbased@users.noreply.github.com>
2023-08-22 20:19:06 +0200

    refactor(content): improve content structure + reorganise files

---
+++ src/content/content.ts
@@ -0,0 +1,43 @@
+import { createWatchLaterButtonElement } from "./watchLaterButton";
+
+const SHARE_BUTTON_SELECTOR = 'button[aria-label="Partager"]';
+
+const handleWatchLaterButtonClick = () => {
+  console.log("Ajouter à regarder plus tard");
+};
+
+const injectWatchLaterButton = (container: Element) => {
+  const watchLaterButtonContainer = createWatchLaterButtonElement(
+    "Watch later",
+    handleWatchLaterButtonClick
+  );
+
+  container.insertBefore(watchLaterButtonContainer, container.firstChild);
+};
+
+const onShareButtonMutation = (shareButton: Element) => {
+  const parentContainer = shareButton.closest(".fbIPLy");
+
+  // watchlater button is already in DOM
+  if (!parentContainer || parentContainer.childNodes.length > 2) return;
+
+  injectWatchLaterButton(parentContainer);
+};
+
+const observer = new MutationObserver((mutationsList) => {
+  for (const mutation of mutationsList) {
+    if (mutation.target instanceof Element) {
+      const shareButton = mutation.target.querySelector(
+        SHARE_BUTTON_SELECTOR
+      ) as HTMLButtonElement | null;
+
+      if (shareButton) {
+        onShareButtonMutation(shareButton);
+      }
+    }
+  }
+});
+
+const observerConfig = { childList: true, subtree: true };
+
+observer.observe(document.body, observerConfig);
