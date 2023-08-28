ab3a39c6ccfc0fc4b3a9a4c6335874b3ad518df4
knowbased <60846047+knowbased@users.noreply.github.com>
2023-08-28 18:27:55 +0200

    style: change watch later button icon

--- src/content/watchLaterButton.ts
+++ src/content/watchLaterButton.ts
@@ -17,9 +17,7 @@ export const createWatchLaterButtonElement = (
                 <div class="Layout-sc-1xcs6mc-0 gzBpxy">
                   <div class="ScCoreButtonIcon-sc-ypak37-0 nHEIR tw-core-button-icon">
                     <div class="ScFigure-sc-wkgzod-0 jNDIBh tw-svg" data-a-selector="tw-core-button-icon">
-                      <svg width="100%" height="100%" viewBox="0 0 20 20" aria-hidden="true">
-                        <path d="M2 16v-3h2v3h12v-3h2v3a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm13-9-1.5 1.5L11 6v7H9V6L6.5 8.5 5 7l5-5 5 5z"></path>
-                      </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><polyline points="12 6 12 12 16 14"/></svg>
                     </div>
                   </div>
                 </div>
