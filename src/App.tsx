7a11dceff9f1d470b1c9e13927d6ed458f4e9049
knowbased <60846047+knowbased@users.noreply.github.com>
2023-09-17 17:48:37 +0200

    feat: change popup content to match with the extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import extensionLogo from "./assets/twitch-watch-later-logo-128x128.png";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      <header className="header">
+        <div className="header-content">
+          <img
+            src={extensionLogo}
+            width="50"
+            className="logo"
+            alt="logo twitch watch later"
+          />
+          <h1>Twitch Watch Later</h1>
+        </div>
+      </header>
+      <main className="main">
+        <section className="instructions">
+          <p>1. Select the video you want to save for later.</p>
+          <p>2. Click on the "Watch Later" button below the player.</p>
+          <p>3. Navigate to the following tab.</p>
+          <p>4. Click on the new "Watch Later" tab.</p>
+          <p>5. Enjoy your saved videos!</p>
+        </section>
+      </main>
+    </div>
   );
 }
 
